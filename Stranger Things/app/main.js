a429914855ddc25d4819f74e17df6ab50a88d5e3
Lucia A <lucia.alvargonzalez@babel.es>
2018-03-02 10:26:55 +0100

    galeria y temporadas añadidas

--- Stranger Things/app/main.js
+++ Stranger Things/app/main.js
@@ -3,15 +3,4 @@ export class Main {
    
 }
 
-function pasarNoticia(){
-    console.log("Entra en funcion")
-    var x = document.getElementById("noticias1");
-    var y =document.getElementById("noticias2");
-if (x.style.display === "none") {
-x.style.display = "block";
-y.style.display = "none";
-} else {
-x.style.display = "none";
-y.style.display = "block"
-}
-}
+
