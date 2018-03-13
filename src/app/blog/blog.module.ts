af394505e18f7200985a52eb0126272279c2b0d4
BeatrizMorenoGarcia <36660072+BeatrizMorenoGarcia@users.noreply.github.com>
2018-03-13 10:58:47 +0100

    Componentes lista y formulario creados

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
+import { ListaComponent } from './lista/lista.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BlogRoutingModule
   ],
-  declarations: [BlogComponent],
+  declarations: [BlogComponent, ListaComponent, FormularioComponent],
   exports: [BlogComponent]
 })
 export class BlogModule { }
