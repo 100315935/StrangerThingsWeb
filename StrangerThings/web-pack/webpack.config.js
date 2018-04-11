deb5f645c433617a2f6dd660b2bb43f5f14a5db3
Lucia A <lucia.alvargonzalez@babel.es>
2018-04-11 12:27:07 +0200

    funciona webpack

--- StrangerThings/web-pack/webpack.config.js
+++ StrangerThings/web-pack/webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: [ '.js', '.jsx']
     },
-    entry: '/app/app.js',
+    entry: './app/app.js',
     output: {
         path: path.resolve(__dirname,'/web-pack'),
         filename: 'bundle.js'
